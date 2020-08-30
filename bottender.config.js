6e1a5086270e253c9b5aeeeaa41ea7ebed06a0a6
Calvin-Lin <calvinlinos@gmail.com>
2020-08-30 19:43:08 +0800

    upgrade packages and add dotenv for dev

--- bottender.config.js
+++ bottender.config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
   session: {
     driver: process.env.NODE_ENV === 'production' ? 'mongo' : 'memory',
@@ -18,8 +20,14 @@ module.exports = {
     line: {
       enabled: true,
       path: '/webhooks/line',
-      accessToken: process.env.LINE_ACCESS_TOKEN,
-      channelSecret: process.env.LINE_CHANNEL_SECRET,
+      accessToken:
+        process.env.NODE_ENV === 'production'
+          ? process.env.LINE_ACCESS_TOKEN
+          : process.env.LINE_ACCESS_TOKEN_DEV,
+      channelSecret:
+        process.env.NODE_ENV === 'production'
+          ? process.env.LINE_CHANNEL_SECRET
+          : process.env.LINE_CHANNEL_SECRET_DEV,
     },
   },
 };
